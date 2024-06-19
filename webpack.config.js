b3309474bfec7c89fb98cf7d0724ee3d4725733a
marcqw <william@marcq.me>
2024-06-19 14:56:28 +0200

    chore: Add Babel configuration and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development', // ou 'production' selon vos besoins
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
