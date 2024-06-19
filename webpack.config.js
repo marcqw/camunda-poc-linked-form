a5fc198b4fdfacac4154b8560deea812a66fe8aa
marcqw <william@marcq.me>
2024-06-19 14:58:12 +0200

    chore: Update webpack configuration to set mode to 'development'

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development', // ou 'production' selon vos besoins
+  mode: 'development',
   module: {
     rules: [
       {
